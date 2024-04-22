c0f01e17844fabfa4e5da360f883087d9214d656
MohamedMostafa <83365873+mo634@users.noreply.github.com>
2024-04-22 09:42:30 +0200

    Yup integration

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -1,26 +1,32 @@
 import React, { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+// define schema for validation 
+
+const schema = yup.object({
+  // define fields and its validation 
+  username: yup.string().required("username is required"),
+  email: yup.string().email().required("email is required"),
+  password: yup.number().min(6).required("password is required")
+}
+)
 const YoutubeForm = () => {
   //states 
 
-  // ************modes ******************
-  //1. onChange => validate the fields  while typing 
-  //2. onBlur => validate the fields  when field is blured 
-  //3. onSubmit => validate the fields  when form is submitted "Default"
-  //4. onTouched => validate the fields  when field is touched 
-  //5. all => validate input field on both "change and bluer"
   const form = useForm({
     defaultValues: {
       username: "",
       email: "mmm@gmail.com",
       password: 123456
     },
-    mode: "onChange"
+
+    // link the schema to the form 
+    resolver: yupResolver(schema),
+
   })
 
-  // dirty : field is modified 
-  // touched : field is touched
 
   const { errors } = form.formState
 
@@ -45,45 +51,15 @@ const YoutubeForm = () => {
 
 
 
-  const handleReset = () => {
-    // reset all only one value
 
-    // setValue("username","" ,{
-    //   // to reflect with  dev tool
-    //   shouldDirty: true,
-    //   shouldTouch: true,
-    //   shouldValidate: true
-    // })
-
-
-    // another way to reset "better to user "
-
-    // reset  value 
-    // form.reset({username: "",})
-
-    // reset all values 
-    form.reset()
-
-  }
 
   const onError = (errors) => {
     console.log(errors)
   }
-  // *********log changes in input field ************
-  useEffect(() => {
-    const subscribe = watch((data) => {
-      console.log(data)
-    })
 
-    return () => subscribe.unsubscribe()
-  }, [watch])
 
   return (
     <div className="form-box">
-      {/* <h1>{username}</h1> */}
-
-
-      {/* no validate to enable your custom validate */}
 
       {/* handleSubmit : accept two funcs , "logic func  and Error func" */}
       <form className="form" onSubmit={handleSubmit(onSubmit, onError)} noValidate>
@@ -94,18 +70,7 @@ const YoutubeForm = () => {
 
           <input type="text" className="input" placeholder="Full Name"
             value={username}
-            {...form.register("username", {
-
-              required: "Please enter your name",
-
-              // add you custom validation 
-              validate: {
-                // not write mohamded in username field input 
-
-                notMohamed: (value) => value !== "mohamed" || "Mohamed is not allowed",
-
-              }
-            })}
+            {...form.register("username")}
           />
 
           {/* render error if exist for username  */}
@@ -114,10 +79,8 @@ const YoutubeForm = () => {
           }
 
           <input type="email" className="input" placeholder="Email"
-
             value={email}
-            {...form.register("email", {
-            })}
+            {...form.register("email")}
           />
 
           {
@@ -128,12 +91,7 @@ const YoutubeForm = () => {
 
           <input type="password" className="input" placeholder="Password"
             value={password}
-            {...form.register("password", {
-              minLength: {
-                value: 6,
-                message: "Password must be at least 6 characters"
-              }
-            })}
+            {...form.register("password")}
           />
 
           {/* render error if exist for password */
@@ -145,10 +103,6 @@ const YoutubeForm = () => {
 
 
         <button>Sign up</button>
-
-        <button
-          onClick={handleReset}
-        >Reset</button>
       </form>
 
       <DevTool control={control} />
